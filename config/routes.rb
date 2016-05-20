7c44c59e6b44dad16087ac57d1f5820778b5220d
Nick Stoneman <nick.stoneman@gmail.com>
2016-05-20 16:13:05 -0400

    changed routes and added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
-Rails.application.routes.draw do
-  
-  root 'static_pages#home'
-  	get 'static_pages/about'
-  	get 'static_pages/contact'
+Rails.application.routes.draw do 
+  root				'static_pages#home'
+  get 'about'	=>	'static_pages#about'
+  get 'contact'	=>	'static_pages#contact'
 
-  get 'controller/ACTION' => 'controller#ACTION'
+  
+  	get 'controller/ACTION' => 'controller#ACTION'
 
   # Static Login Pages
-  	get 'register_page/register' => 'register_page#register'
-  	get 'login_page/login' => 'login_page#login'
+  	get 'register' => 'register_page#register'
+  	get 'login' => 'login_page#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
