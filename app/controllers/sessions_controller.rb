86c4b79c2e26d380564bfba48bb28042ac052fef
Nick Stoneman <nick.stoneman@gmail.com>
2016-06-14 11:23:39 -0400

    added ability to destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,16 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		log_in user
   		redirect_to user
-  		# Log the user in and redirect to the user's show page
+  		# Log the user in and redirect to the user's show page.
   	else
-  		# Create an error message
+  		# Create an error message.
   		flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
     	render 'new'
   	end
 	end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
-
 end
