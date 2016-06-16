12a2b77b3883f0c71b17c25cde54248db8ece4ab
Nick Stoneman <nick.stoneman@gmail.com>
2016-06-16 11:33:51 -0400

    added remember checkbox to login button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		# redirects the user to User show.html.erb
       redirect_to user
   		# Log the user in and redirect to the user's show page.
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 	end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
